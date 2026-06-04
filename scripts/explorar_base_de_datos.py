074d893e0a45674cf73a2a1bcf27496cc427cfe2
Lucas Wulff <lucasd.wulff@gmail.com>
2026-06-04 13:45:19 -0300

    Regresion lineal Multiple terminada! dea

--- scripts/explorar_base_de_datos.py
+++ scripts/explorar_base_de_datos.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import sys
+import statsmodels.api as sm
+
 
 
 RUTA_PROYECTO = Path(__file__).resolve().parents[1]
 sys.path.append(str(RUTA_PROYECTO))
-
 from src.carga_datos import CargaDatos
+
 
 
 COLUMNAS_NUMERICAS = [
@@ -37,25 +39,24 @@ COLUMNAS_NUMERICAS = [
 ]
 
 
-def main():
-    ruta_datos = RUTA_PROYECTO / "data" / "jugadores_2024_2025.csv"
-    cargador = CargaDatos(ruta_datos)
 
-    cargador.cargar_datos()
-    cargador.mostrar_dimensiones()
+ruta_datos = RUTA_PROYECTO / "data" / "jugadores_2024_2025.csv"
+cargador = CargaDatos(ruta_datos)
 
-    print()
-    cargador.mostrar_primeras_filas(n=5)
+cargador.cargar_datos()
+cargador.mostrar_dimensiones()
 
-    print()
-    cargador.mostrar_informacion()
+print()
+cargador.mostrar_primeras_filas(n=5)
 
-    print()
-    cargador.mostrar_valores_faltantes(n=10)
+print()
+cargador.mostrar_informacion()
 
-    print()
-    cargador.mostrar_resumen_numerico(columnas=COLUMNAS_NUMERICAS)
+print()
+cargador.mostrar_valores_faltantes(n=10)
 
+print()
+cargador.mostrar_resumen_numerico(columnas=COLUMNAS_NUMERICAS)
 
-if __name__ == "__main__":
-    main()
+#Debemos entrenar un modelo con Regresión Lineal Multiple, por lo tanto vamos a buscar las 
+#Variables que resulten mas llamativas para
