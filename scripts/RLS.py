d80b32ee65e50ba5f23523d692615f577139ec5c
soficami <sofcamilletti6@gmail.com>
2026-06-04 10:16:14 -0300

    Modelos cuando hicieron al menos un gol

--- scripts/RLS.py
+++ scripts/RLS.py
@@ -16,11 +16,18 @@ cargador = CargaDatos(ruta_datos)
 df = cargador.cargar_datos()
 
 # Regresion Lineal Simple.
-'''Para predecir la cantidad de goles, tomaremos las variables Sh (tiros totales), SoT (tiros al arco) 
-y Min (minutos jugados) para hacer, por separado, cada una de las regresiones lineales simples.'''
+'''
+En esta primera parte, debemos predecir la cantidad de goles mediante un modelo de regresion
+lineal simple. Para hacerlo utlizando una unica variable, tomaremos las variables Sh (tiros totales), SoT (tiros al arco) 
+y Min (minutos jugados) para hacer, por separado, cada uno de los modelos.
+Decidimos utilizar estas variables porque supusimos que, a mayor cantidad de tiros (totales y al arco), mas probable es 
+que hagan goles. El mismo razonamiento utilizamos para seleccionar minutos jugados (a mayor tiempo jugado, mayor posibilidad de meter un gol).
+'''
 
 # df = df[df['Gls'] > 0]
 y = df['Gls']
+
+'''
 # Variable predictora: tiros totales
 x = np.array(df['Sh'])
 X = sm.add_constant(x)
@@ -30,6 +37,7 @@ print(result1.summary())
 
 plt.figure()
 sns.scatterplot(data=df, x='Sh', y=y)
+plt.title('Tiros totales')
 plt.show()
 
 # Variable predictora: tiros al arco
@@ -41,6 +49,7 @@ print(result2.summary())
 
 plt.figure()
 sns.scatterplot(data=df, x='SoT', y=y)
+plt.title('Tiros al arco')
 plt.show()
 
 # Variable predictora: minutos jugados
@@ -52,7 +61,12 @@ print(result3.summary())
 
 plt.figure()
 sns.scatterplot(data=df, x='Min', y=y)
+plt.title('Minutos jugados')
 plt.show()
 
-'''Comparando los R-ajustados de cada modelo, vemos que tiros al arco (0.726) y tiros totales (0.814) se ajustan, siendo este último la mejor
-de las tres variables. Mientras que minutos jugados no; con un R-ajustado de 0.162'''
+
+Comparando los R-ajustados de cada modelo, vemos que tiros al arco (0.726) y tiros totales (0.814) se ajustan, siendo este último la mejor
+de las tres variables. Mientras que minutos jugados no; con un R-ajustado de 0.162. 
+'''
+
+print((df['Gls'] !=0).sum())
